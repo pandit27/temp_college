ef81b43529b01dec1f72eb33424aab190387e534
Priyanshu Jha <95862487+AstroDeveloper1010@users.noreply.github.com>
2023-02-10 02:24:14 +0530

    Create dfs.cpp

---
+++ design_and_analaysis_of_algorithm/Graph/dfs.cpp
@@ -0,0 +1,44 @@
+// #include <bits/stdc++.h>
+#include <iostream>
+#include <map>
+#include <vector>
+#include <queue>
+using namespace std;
+
+// class to implement depth first search algorithm
+class depthFirstSearch
+{
+private:
+    void dfs(int node, map<int, vec<int>> &m, int vis[], list<int> &ls){
+        vis[node] = 1;
+        ls.push_back(node);
+        
+        // traverse its neighbours
+        for(auto it : m[node]){
+            if(vis[it] == 0){
+                dfs(it, m, vis, ls);
+            }
+        }
+    }
+    
+public:
+    vector<int> dfs_traversal(int v, map<int, vec<int>> &m){
+        int vis[v] = {0};
+        int start = 0;
+        list<int> ls;
+        
+        dfs(start, m, vis, ls);
+        
+        return ls;
+    }
+};
+
+int main()
+{
+    int v;
+    cout << "Enter the number of vertices: ";
+    cin >> v;
+
+
+    return 0;
+}
