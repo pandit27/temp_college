26c4465ca50a7b5dc9f3ffd3a90fab9e6508c8cb
Priyanshu Jha <priyanshujha131@gmail.com>
2023-02-24 17:29:04 +0530

    updated

--- design_and_analaysis_of_algorithm/temp_learning/bfs_traversal.cpp
+++ design_and_analaysis_of_algorithm/temp_learning/bfs_traversal.cpp
@@ -1,3 +1,6 @@
+// C++ code to implement bfs traverasal in a graph
+// Code written by Priyanshu Jha
+
 #include <iostream>
 #include <vector>
 #include <map>
@@ -11,8 +14,8 @@ public:
 
     void addEdge(int a, int b)
     {
-        m[a].push_back(b);
-        m[b].push_back(a);
+        m[a].push_back(b); // add edge from a to b
+        m[b].push_back(a); // add edge from b to a (undirected graph)
     }
 
     vector<int> bfs_path(int v)
@@ -20,54 +23,54 @@ public:
         vector<int> bfs;       // vector to store bfs path
         vector<int> vis(v, 0); // vector to find if a node is visited or not
 
-        for (int i = 0; i < v; i++)
+        for (int i = 0; i < v; i++) // loop over all nodes (0-based indexing)
         {
-            if (vis[i] == 0)
+            if (vis[i] == 0) // if the node hasn't been visited yet
             {
                 queue<int> q; // queue to add the nodes in the queue to perform pop() operation and find the bfs path
 
-                q.push(i);
-                vis[i] = 1;
+                q.push(i);  // add the starting node to the queue
+                vis[i] = 1; // mark the starting node as visited
 
-                while (!q.empty())
+                while (!q.empty()) // while there are still nodes in the queue
                 {
-                    int top = q.front();
-                    q.pop();
+                    int top = q.front(); // take the first node in the queue
+                    q.pop();             // remove the first node from the queue
 
-                    bfs.push_back(top);
+                    bfs.push_back(top); // add the node to the bfs vector to keep track of the order in which nodes are visited
 
-                    for (auto it : m[top])
+                    for (auto it : m[top]) // loop over all neighbors of the current node
                     {
-                        if (vis[it] == 0)
+                        if (vis[it] == 0) // if the neighbor hasn't been visited yet
                         {
-                            q.push(it);
-                            vis[it] = 1;
+                            q.push(it);  // add the neighbor to the queue
+                            vis[it] = 1; // mark the neighbor as visited
                         }
                     }
                 }
             }
         }
 
-        return bfs;
+        return bfs; // return the bfs vector containing the order in which nodes were visited during the traversal
     }
 };
 
 int main()
 {
-    int v = 7;
+    int v = 10; // number of nodes in the graph
 
-    solution s;
-    s.addEdge(1, 4);
-    s.addEdge(2, 9);
-    s.addEdge(6, 8);
-    s.addEdge(4, 3);
-    s.addEdge(2, 7);
-    s.addEdge(8, 9);
-    s.addEdge(1, 10);
+    solution s;      // create a new solution object
+    s.addEdge(0, 3); // add edges to the graph
+    s.addEdge(1, 8);
+    s.addEdge(5, 7);
+    s.addEdge(3, 2);
+    s.addEdge(1, 6);
+    s.addEdge(7, 8);
+    s.addEdge(0, 9);
 
-    vector<int> vec = s.bfs_path(v);
+    vector<int> vec = s.bfs_path(v); // perform BFS traversal starting from node 0
 
-    for (auto it : vec)
+    for (auto it : vec) // print out the order in which nodes were visited during the traversal
     {
         cout << it << endl;
     }
