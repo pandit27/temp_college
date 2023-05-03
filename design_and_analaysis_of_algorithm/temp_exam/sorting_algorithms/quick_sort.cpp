0560d53a889d3a3c16c61d68e52b8679b7c80440
Priyanshu Jha <priyanshujha131@gmail.com>
2023-05-03 15:19:05 +0530

    udpated

--- design_and_analaysis_of_algorithm/temp_exam/sorting_algorithms/quick_sort.cpp
+++ design_and_analaysis_of_algorithm/temp_exam/sorting_algorithms/quick_sort.cpp
@@ -1,21 +1,26 @@
+// C++ program to implement quick sort
 #include <iostream>
 #include <vector>
 #include <utility>
 
 using namespace std;
 
-class Solution {
-    public:
-        void partition(vector<int> &v);
-        void sort(vector<int> &v);
+class Solution
+{
+public:
+    void partition(vector<int> &v);
+    void sort(vector<int> &v);
 };
 
-int main() {
+int main()
+{
     int n;
-    cout<<"Enter the size of the array: "; cin>>n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
     vector<int> v;
-    cout<<"Enter the elements of the array: \n";
-    for(int i = 0; i < n; i++) {
+    cout << "Enter the elements of the array: \n";
+    for (int i = 0; i < n; i++)
+    {
         int temp;
         v.push_back(temp);
     }
@@ -23,10 +28,11 @@ int main() {
     Solution s;
     s.sort(v);
 
-    cout<<"After sorting \n";
-    
-    for(auto it : v) {
-        cout<<v[it]<<" ";
+    cout << "After sorting \n";
+
+    for (auto it : v)
+    {
+        cout << v[it] << " ";
     }
 
     return 0;
