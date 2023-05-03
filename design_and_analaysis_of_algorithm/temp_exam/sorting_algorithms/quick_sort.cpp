6a778d8611325321a9e16e2ee9af5176487bcb2c
Priyanshu Jha <priyanshujha131@gmail.com>
2023-05-03 15:17:43 +0530

    updated

---
+++ design_and_analaysis_of_algorithm/temp_exam/sorting_algorithms/quick_sort.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+
+using namespace std;
+
+class Solution {
+    public:
+        void partition(vector<int> &v);
+        void sort(vector<int> &v);
+};
+
+int main() {
+    int n;
+    cout<<"Enter the size of the array: "; cin>>n;
+    vector<int> v;
+    cout<<"Enter the elements of the array: \n";
+    for(int i = 0; i < n; i++) {
+        int temp;
+        v.push_back(temp);
+    }
+
+    Solution s;
+    s.sort(v);
+
+    cout<<"After sorting \n";
+    
+    for(auto it : v) {
+        cout<<v[it]<<" ";
+    }
+
+    return 0;
+}
