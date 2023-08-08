5daba91cc182ef1790dd8e342c9ddae0ed0ef29f
Priyanshu Jha <priyanshujha131@gmail.com>
2023-08-08 20:18:40 +0530

    created practical 3

--- design_and_analaysis_of_algorithm/Knapsack Problems/knapsack_problem.cpp
+++ design_and_analaysis_of_algorithm/Knapsack Problems/knapsack_problem.cpp
@@ -5,12 +5,15 @@
 #include <vector>
 using namespace std;
 
-int knapsack(const vector<int>& values, const vector<int>& weights, int capacity) {
+int knapsack(const vector<int> &values, const vector<int> &weights, int capacity)
+{
     int n = values.size();
-    vector<int> dp(capacity+1);
+    vector<int> dp(capacity + 1);
 
-    for (int i = 0; i < n; i++) {
-        for (int w = capacity; w >= weights[i]; w--) {
+    for (int i = 0; i < n; i++)
+    {
+        for (int w = capacity; w >= weights[i]; w--)
+        {
             dp[w] = max(dp[w], dp[w - weights[i]] + values[i]);
         }
     }
@@ -18,7 +21,8 @@ int knapsack(const vector<int>& values, const vector<int>& weights, int capacity
     return dp[capacity];
 }
 
-int main() {
+int main()
+{
     vector<int> values = {20, 5, 10, 40, 15, 25};
     vector<int> weights = {1, 2, 3, 8, 7, 4};
     int capacity = 10;
