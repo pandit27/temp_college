afd7b952c36b98bdd11a1626f838e0f04f2ecef4
Priyanshu Jha <priyanshujha131@gmail.com>
2023-08-09 15:58:23 +0530

    created practical_6

---
+++ internet_technologies_sem5/practical_5/script.js
@@ -0,0 +1,47 @@
+// declare variables
+const para = document.querySelector('.para');
+const red = document.querySelector('.red');
+const green = document.querySelector('.green');
+const blue = document.querySelector('.blue');
+const ten = document.querySelector('.ten');
+const twenty = document.querySelector('.twenty');
+const thirty = document.querySelector('.thirty');
+const cursive = document.querySelector('.cursive');
+const cnew = document.querySelector('.cnew');
+const tnr = document.querySelector('.tnr');
+
+red.addEventListener('click', (e) => {
+    para.style.color = 'red';
+});
+
+blue.addEventListener('click', (e) => {
+    para.style.color = 'blue';
+});
+
+green.addEventListener('click', (e) => {
+    para.style.color = 'green';
+});
+
+ten.addEventListener('click', (e) => {
+    para.style.fontSize = 10;
+});
+
+twenty.addEventListener('click', (e) => {
+    para.style.fontSize = 20;
+});
+
+thirty.addEventListener('click', (e) => {
+    para.style.fontSize = 30;
+});
+
+cursive.addEventListener('click', (e) => {
+    para.style.fontFamily = 'cursive';
+});
+
+cnew.addEventListener('click', (e) => {
+    para.style.fontFamily = 'Courier New';
+});
+
+tnr.addEventListener('click', (e) => {
+    para.style.fontFamily = 'Times New Roman';
+});
